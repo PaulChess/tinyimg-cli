5fea653b65ebe03f59e1848bf7a0feb1d72da616
paulChess <sjq15251852316@163.com>
2021-03-30 21:25:36 +0800

    完成图片压缩入口恒旭

--- bin/tinyimg.js
+++ bin/tinyimg.js
@@ -1,12 +1,52 @@
 #!/usr/bin/env node
+/**
+ * @author shenjiaqi
+ * @date 2021-03-30
+ * @description 图片压缩入口程序
+ */
 
 const Liftoff = require('liftoff');
+const argv = require('minimist')(process.argv.slice(2));
+const version = require('../package.json').version;
+const commander = require('commander');
+const { exec } = require('child_process');
+console.log(process.argv.slice(2));
 
 const cli = new Liftoff({
-  name: 'tinyimg',
-  processTitle: 'tinyimg',
-  moduleName: 'tinyimg',
+  name: 'spress',
+  precessTitle: 'spress',
+  moduleName: 'spress',
   extensions: {
     '.js': null
   }
+})
+
+class App {
+  register(commander, argv) {
+    commander.version(version, '-v, --version');
+    if (argv._.length === 0) {
+      exec('tinyimg --help', (err, info) => {
+        console.log(info);
+      })
+    }
+    const program = commander
+        .description('automatic download images from tinypng.com')
+        .option('-p, --path <value>', 'tinypimg image path', '')
+        .option('-c, --count <value>', 'compress frequency', '')
+        .option('-d, --deep', 'deeploop director', '');
+    // action
+    program.action(() => {
+      console.log('action');
+    });
+    commander.parse(process.argv);
+  }
+}
+
+
+cli.launch({
+  cwd: argv.r || argv.root
+}, (env) => {
+  // liftoff回调中提供env环境变量
+  const appInstance = new App();
+  appInstance.register(commander, argv, env);
 })
